8e906e9f267fb51c5cffa1f5cec42012b2cdfd9b
david <da.larenas@duocuc.cl>
2025-10-16 20:12:01 -0300

    Agregue el login y el registo, en el login realice los formularios del email como de contraseña agregando tambien el logo de la tienda y el boton de inicio de sesion, tambien una redireccion que ira hacia el registo. En el registro se agreganron todos los formularios necesarios (nombre, email, contraseña, etc) y el boton para regiistrarse.  falta la logica para poder guardar los usuarios registrados y tambien la redireccion para la pagina principal de la aplicacion (el home) como tambien algunos detalles visuales

--- app/src/main/java/cl/duoc/levelupgamer/MainActivity.kt
+++ app/src/main/java/cl/duoc/levelupgamer/MainActivity.kt
@@ -4,13 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import cl.duoc.levelupgamer.ui.LoginScreen
+import cl.duoc.levelupgamer.ui.RegistrationScreen
 import cl.duoc.levelupgamer.ui.theme.LevelUpGamerTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +17,16 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LevelUpGamerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = "login") {
+                    composable("login") {
+                        LoginScreen(onRegisterClick = { navController.navigate("register") })
+                    }
+                    composable("register") {
+                        RegistrationScreen()
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LevelUpGamerTheme {
-        Greeting("Android")
     }
 }
