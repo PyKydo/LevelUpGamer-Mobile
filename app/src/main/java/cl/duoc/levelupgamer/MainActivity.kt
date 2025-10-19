baec19eb74a7b31bb23b85c16b7d5d91af962d57
victo <vi.mena@duocuc.cl>
2025-10-19 18:27:39 -0300

    Implementacion de flujo de navegacion entre las pantallas registro y incio de session

--- app/src/main/java/cl/duoc/levelupgamer/MainActivity.kt
+++ app/src/main/java/cl/duoc/levelupgamer/MainActivity.kt
@@ -1,23 +1,11 @@
 package cl.duoc.levelupgamer
 
-import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.annotation.RequiresApi
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import cl.duoc.levelupgamer.model.repository.AuthRepository
-import cl.duoc.levelupgamer.ui.LoginScreen
-import cl.duoc.levelupgamer.ui.RegistrationScreen
+import cl.duoc.levelupgamer.ui.navigation.AppNavigation
 import cl.duoc.levelupgamer.ui.theme.LevelUpGamerTheme
-import androidx.lifecycle.viewmodel.compose.viewModel
-import cl.duoc.levelupgamer.viewmodel.LoginViewModel
-import cl.duoc.levelupgamer.viewmodel.LoginViewModelFactory
-import cl.duoc.levelupgamer.viewmodel.RegistrationViewModel
-import cl.duoc.levelupgamer.viewmodel.RegistrationViewModelFactory
 
 class MainActivity : ComponentActivity() {
     
@@ -26,31 +14,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LevelUpGamerTheme {
-                val authRepository = AuthRepository()
-                val navController = rememberNavController()
-                NavHost(navController = navController, startDestination = "login") {
-                    composable("login") {
-                        val vm: LoginViewModel = viewModel(factory = LoginViewModelFactory(authRepository))
-                        LoginScreen(
-                            vm = vm,
-                            onRegisterClick = { navController.navigate("register") },
-                            onLoggedIn = {
-                                // Aun nada
-                            }
-                        )
-                    }
-                    composable("register") {
-                        val vm: RegistrationViewModel = viewModel(factory = RegistrationViewModelFactory(authRepository))
-                        RegistrationScreen(
-                            vm = vm,
-                            onRegistered = {
-                                // Tras registro exitoso, volver a login
-                                navController.popBackStack()
-                            },
-                            onGoToLogin = { navController.popBackStack() }
-                        )
-                    }
-                }
+                AppNavigation() // Aquí se carga toda la navegación
             }
         }
     }
