5e548b04ff70042c434ec1398bb9fda90531689e
david <da.larenas@duocuc.cl>
2025-10-19 18:59:34 -0300

    Realice una transicion cuando el usuario va al registro desde el inicio de sesion y vicerversa

--- app/src/main/java/cl/duoc/levelupgamer/MainActivity.kt
+++ app/src/main/java/cl/duoc/levelupgamer/MainActivity.kt
@@ -1,11 +1,13 @@
 package cl.duoc.levelupgamer
 
-import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.annotation.RequiresApi
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.tween
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -13,14 +15,13 @@ import cl.duoc.levelupgamer.model.repository.AuthRepository
 import cl.duoc.levelupgamer.ui.LoginScreen
 import cl.duoc.levelupgamer.ui.RegistrationScreen
 import cl.duoc.levelupgamer.ui.theme.LevelUpGamerTheme
-import androidx.lifecycle.viewmodel.compose.viewModel
 import cl.duoc.levelupgamer.viewmodel.LoginViewModel
 import cl.duoc.levelupgamer.viewmodel.LoginViewModelFactory
 import cl.duoc.levelupgamer.viewmodel.RegistrationViewModel
 import cl.duoc.levelupgamer.viewmodel.RegistrationViewModelFactory
 
 class MainActivity : ComponentActivity() {
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -28,8 +29,25 @@ class MainActivity : ComponentActivity() {
             LevelUpGamerTheme {
                 val authRepository = AuthRepository()
                 val navController = rememberNavController()
-                NavHost(navController = navController, startDestination = "login") {
-                    composable("login") {
+                NavHost(
+                    navController = navController, 
+                    startDestination = "login"
+                ) {
+                    composable(
+                        "login",
+                        exitTransition = {
+                            slideOutOfContainer(
+                                towards = AnimatedContentTransitionScope.SlideDirection.Left, 
+                                animationSpec = tween(700)
+                            )
+                        },
+                        popEnterTransition = {
+                            slideIntoContainer(
+                                towards = AnimatedContentTransitionScope.SlideDirection.Right, 
+                                animationSpec = tween(700)
+                            )
+                        }
+                    ) {
                         val vm: LoginViewModel = viewModel(factory = LoginViewModelFactory(authRepository))
                         LoginScreen(
                             vm = vm,
@@ -39,12 +57,25 @@ class MainActivity : ComponentActivity() {
                             }
                         )
                     }
-                    composable("register") {
+                    composable(
+                        "register",
+                        enterTransition = { 
+                            slideIntoContainer(
+                                towards = AnimatedContentTransitionScope.SlideDirection.Left, 
+                                animationSpec = tween(700)
+                            )
+                        },
+                        popExitTransition = { 
+                            slideOutOfContainer(
+                                towards = AnimatedContentTransitionScope.SlideDirection.Right, 
+                                animationSpec = tween(700)
+                            )
+                        }
+                    ) {
                         val vm: RegistrationViewModel = viewModel(factory = RegistrationViewModelFactory(authRepository))
                         RegistrationScreen(
                             vm = vm,
                             onRegistered = {
-                                // Tras registro exitoso, volver a login
                                 navController.popBackStack()
                             },
                             onGoToLogin = { navController.popBackStack() }
