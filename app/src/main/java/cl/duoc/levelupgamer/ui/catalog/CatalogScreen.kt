b207a3c127e330f05eb2a54c5e4ccf05913ccebc
victo <vi.mena@duocuc.cl>
2025-10-18 23:52:50 -0300

    Se implemento la navegacion para gestionar el fljo entre pantallas

---
+++ app/src/main/java/cl/duoc/levelupgamer/ui/catalog/CatalogScreen.kt
@@ -0,0 +1,10 @@
+package cl.duoc.levelupgamer.ui.catalog
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun CatalogScreen(navController: NavController) {
+    Text(text = "Catalog Screen")
+}
