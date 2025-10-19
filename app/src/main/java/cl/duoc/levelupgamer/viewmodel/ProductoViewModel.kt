9f585f55441a179ed0f5b8f1a510b2a4fb283403
Matías Gutiérrez <mattygh2017@gmail.com>
2025-10-19 20:22:20 -0300

    Eliminada la clase ProductoViewModelFactory del archivo ProductoViewModel.kt para simplificar la creación del ViewModel.

--- app/src/main/java/cl/duoc/levelupgamer/viewmodel/ProductoViewModel.kt
+++ app/src/main/java/cl/duoc/levelupgamer/viewmodel/ProductoViewModel.kt
@@ -18,11 +18,3 @@ class ProductoViewModel(private val repo: ProductoRepository) : ViewModel() {
             initialValue = emptyList()
         )
 }
-
-class ProductoViewModelFactory(private val db: AppDatabase) : ViewModelProvider.Factory {
-    @Suppress("UNCHECKED_CAST")
-    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        val repo = ProductoRepository(db.productoDao())
-        return ProductoViewModel(repo) as T
-    }
-}
