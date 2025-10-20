5577feff01771689c15e7987386a4745bb45662f
Matías Gutiérrez <mattygh2017@gmail.com>
2025-10-20 02:40:28 -0300

    Eliminación de colores no utilizados en Color.kt

--- app/src/main/java/cl/duoc/levelupgamer/ui/theme/Color.kt
+++ app/src/main/java/cl/duoc/levelupgamer/ui/theme/Color.kt
@@ -2,15 +2,6 @@ package cl.duoc.levelupgamer.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
-
-// Paleta de Level-Up Gamer
 val Black = Color(0xFF000000)         // Negro puro para fondos
 val ElectricBlue = Color(0xFF1E90FF)    // Azul Eléctrico
 val NeonGreen = Color(0xFF39FF14)       // Verde Neón
